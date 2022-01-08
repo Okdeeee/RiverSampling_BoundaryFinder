93bb87130e212f373e7ad2ab4fc4c46ec085ad2d
od <od19961110@gmail.com>
2022-01-08 15:31:52 +0800

    新增Type參數

--- River_splitpoint_sampling.py
+++ River_splitpoint_sampling.py
@@ -18,6 +18,7 @@ river_code_field = arcpy.GetParameter(2)
 split_dist = arcpy.GetParameter(3)
 extend_dist = arcpy.GetParameter(4)
 sampling_distance = arcpy.GetParameter(5)
+sampleType = arcpy.GetParameterAsText(6)
 
 arcpy.env.workspace = os.path.dirname(ouptut_layer_path)
 arcpy.env.overwriteOutput = True
@@ -39,6 +40,12 @@ def points_along_line(line_lyr, pnt_layer, pnt_dist, extend_dist) :
 
 	search_cursor = arcpy.da.SearchCursor(line_lyr, field_names= ['SHAPE@', river_code_field.value])
 	insert_cursor = arcpy.da.InsertCursor(pnt_layer, field_names= ['SHAPE@', 'Code'])
+	if sampleType == 'Horizontal' :
+		r = 90
+		l = 270
+	elif sampleType == 'Vertical' :
+		r = 0
+		l = 180
 
 	for row in search_cursor:
 		for n, dist in enumerate(np.arange(0, row[0].length, pnt_dist)):
@@ -47,11 +54,11 @@ def points_along_line(line_lyr, pnt_layer, pnt_dist, extend_dist) :
 			# insert_cursor.insertRow([arcpy.Polyline(arcpy.Array([point.pointFromAngleAndDistance(90, extend_dirt, 'PLANAR').firstPoint, point.firstPoint, point.pointFromAngleAndDistance(270, extend_dirt, 'GEODESIC').firstPoint]))])
 			for count, sample in enumerate(np.arange(0 ,extend_dist, sampling_distance)) :
 				if sample == 0.0 :
-					r_point = point.pointFromAngleAndDistance(90, sampling_distance, 'PLANAR')
-					l_point = point.pointFromAngleAndDistance(270, sampling_distance, 'PLANAR')
+					r_point = point.pointFromAngleAndDistance(r, sampling_distance, 'PLANAR')
+					l_point = point.pointFromAngleAndDistance(l, sampling_distance, 'PLANAR')
 				else :
-					r_point = r_point.pointFromAngleAndDistance(90, sampling_distance, 'PLANAR')
-					l_point = l_point.pointFromAngleAndDistance(270, sampling_distance, 'PLANAR')
+					r_point = r_point.pointFromAngleAndDistance(r, sampling_distance, 'PLANAR')
+					l_point = l_point.pointFromAngleAndDistance(l, sampling_distance, 'PLANAR')
 				insert_cursor.insertRow([r_point, '{}{:>04}+{:>04}'.format(row[1], n, count+1)])
 				insert_cursor.insertRow([l_point, '{}{:>04}-{:>04}'.format(row[1], n, count+1)])
 
--- River_splitpoint_sampling_py3.py
+++ River_splitpoint_sampling_py3.py
@@ -12,17 +12,18 @@ import numpy as np
 # sampling_distance = arcpy.GetParameter(6)
 
 river_layer = arcpy.GetParameter(0)
-ouptut_layer_path = arcpy.GetParameter(1)
+ouptut_layer_path = arcpy.GetParameterAsText(1)
 # in_raster = arcpy.GetParameter(2)
 river_code_field = arcpy.GetParameter(2)
 split_dist = arcpy.GetParameter(3)
 extend_dist = arcpy.GetParameter(4)
 sampling_distance = arcpy.GetParameter(5)
+sampleType = arcpy.GetParameterAsText(6)
 
-arcpy.env.workspace = os.path.dirname(ouptut_layer_path.value)
+arcpy.env.workspace = os.path.dirname(ouptut_layer_path)
 arcpy.env.overwriteOutput = True
 
-root_path = os.path.dirname(ouptut_layer_path.value)
+root_path = os.path.dirname(ouptut_layer_path)
 #	Code = [AA][0000][+/-0000]
 #	製作河流取樣點圖層
 def points_along_line(line_lyr, pnt_layer, pnt_dist, extend_dist) :
@@ -32,12 +33,18 @@ def points_along_line(line_lyr, pnt_layer, pnt_dist, extend_dist) :
   pnt_dist (integer) - Interval distance in map units to add points
   """
   if not arcpy.Exists(pnt_layer):
-    arcpy.CreateFeatureclass_management(os.path.dirname(pnt_layer.value), ouptut_layer_path, 'POINT', spatial_reference=arcpy.SpatialReference(3826))
+    arcpy.CreateFeatureclass_management(os.path.dirname(pnt_layer), os.path.basename(pnt_layer), 'POINT', spatial_reference=arcpy.SpatialReference(3826))
 
   arcpy.AddField_management(ouptut_layer_path, 'Code', 'TEXT', field_scale=20)
 
   search_cursor = arcpy.da.SearchCursor(line_lyr, field_names= ['SHAPE@', river_code_field.value])
   insert_cursor = arcpy.da.InsertCursor(ouptut_layer_path, field_names= ['SHAPE@', 'Code'])
+  if sampleType == 'Horizontal' :
+    r = 90
+    l = 270
+  elif sampleType == 'Vertical' :
+    r = 0
+    l = 180
 
   for row in search_cursor:
     for n, dist in enumerate(np.arange(0, row[0].length, pnt_dist)):
@@ -46,11 +53,11 @@ def points_along_line(line_lyr, pnt_layer, pnt_dist, extend_dist) :
       # insert_cursor.insertRow([arcpy.Polyline(arcpy.Array([point.pointFromAngleAndDistance(90, extend_dirt, 'PLANAR').firstPoint, point.firstPoint, point.pointFromAngleAndDistance(270, extend_dirt, 'GEODESIC').firstPoint]))])
       for count, sample in enumerate(np.arange(0 ,extend_dist, sampling_distance)) :
         if sample == 0.0 :
-          r_point = point.pointFromAngleAndDistance(90, sampling_distance, 'PLANAR')
-          l_point = point.pointFromAngleAndDistance(270, sampling_distance, 'PLANAR')
+          r_point = point.pointFromAngleAndDistance(r, sampling_distance, 'PLANAR')
+          l_point = point.pointFromAngleAndDistance(l, sampling_distance, 'PLANAR')
         else :
-          r_point = r_point.pointFromAngleAndDistance(90, sampling_distance, 'PLANAR')
-          l_point = l_point.pointFromAngleAndDistance(270, sampling_distance, 'PLANAR')
+          r_point = r_point.pointFromAngleAndDistance(r, sampling_distance, 'PLANAR')
+          l_point = l_point.pointFromAngleAndDistance(l, sampling_distance, 'PLANAR')
         insert_cursor.insertRow([r_point, f'{row[1]}{n:>04}+{count+1:>04}'])
         insert_cursor.insertRow([l_point, f'{row[1]}{n:>04}-{count+1:>04}'])
 
